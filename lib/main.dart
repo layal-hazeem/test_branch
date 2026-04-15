884fcc8a56759db19c872f8ca8f64e8529d2ba5e
joudy-sal <joudysal264@gmail.com>
2026-04-15 16:50:27 +0300

    add print

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  print("Hello from joudyy ");
+  print("Hello from joudy ");
+  print("Test Test thr merge from joudy  ");
+
 
   runApp(const MyApp());
   print('branch of Masa');
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("Test ")),
+        appBar: AppBar(title: const Text("Test joudy")),
         body: const Center(child: Text("Hello from main last edit ")),
       ),
     );
