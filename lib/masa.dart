c8d5fda913c81d64fef2cb6f0a4b11b4c13fa8c7
joudy-sal <joudysal264@gmail.com>
2026-04-15 18:11:55 +0300

    add print

--- lib/masa.dart
+++ lib/masa.dart
@@ -3,14 +3,14 @@ class Masa {
   String name;
   int name_joudy;
   bool yes;
+  String des;
   int age;
 
   Masa({
     required this.name,
     required this.yes,
     required this.name_joudy,
+    required this.des,
     required this.age,
   });
-  String des;
-  Masa({required this.name, required this.yes, required this.name_joudy,required this.des});
 }
